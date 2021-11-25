f531455373d49ffd4416cd0248b00db78ba72388
Ornella Fifa <ornellafifa@gmail.com>
2021-11-25 11:56:44 +0100

    Creation de l'activité

---
+++ app/src/main/java/Ornella242/demo/CalculatriceActivity.java
@@ -0,0 +1,14 @@
+package Ornella242.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
